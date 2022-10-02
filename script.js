9ab3df0709e4842cbb5949a2382ad5e4e99f5921
Prashant Bhardwaj <geekyprashant840@gmail.com>
2022-10-02 23:09:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Settings Menu Toggle
+var settingsmenu = document.querySelector(".settings-menu");
+function settingsMenuToggle() {
+  settingsmenu.classList.toggle("settings-menu-height");
+}
+
+// Dark Button Toggle
+var darkBtn = document.getElementById("dark-btn");
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
+
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  darkBtn.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
